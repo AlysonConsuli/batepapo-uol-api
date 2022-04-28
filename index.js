363a07d541807eaaa2aae8083c16a00e1a573ca0
Alyson Consuli <consulialyson@gmail.com>
2022-04-28 15:04:21 -0300

    feat: post participants recebe lastStatus & adiciona uma mensagem na colecao messages, ja com o dayjs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, { json } from 'express'
 import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
+import dayjs from 'dayjs'
 
 const app = express()
 app.use(cors())
@@ -11,18 +12,25 @@ dotenv.config()
 let db = null
 const mongoClient = new MongoClient(process.env.MONGO_URI)
 
-app.get('/', (req, res) => {
-    res.send('Server is running on: http://localhost:5000')
-})
-
 app.post('/participants', async (req, res) => {
     try {
         const { name } = req.body
         await mongoClient.connect()
-        db = mongoClient.db("uol")
+        db = mongoClient.db('uol')
         const participants = db.collection('participants')
-        await participants.insertOne({ name })
-        res.status(201).send('Created')
+        const messages = db.collection('messages')
+        await participants.insertOne({
+            name,
+            lastStatus: Date.now()
+        })
+        await messages.insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        })
+        res.sendStatus(201)
         mongoClient.close()
     } catch {
         res.status(400).send('Erro')
@@ -33,10 +41,28 @@ app.post('/participants', async (req, res) => {
 app.get('/participants', async (req, res) => {
     try {
         await mongoClient.connect()
-        db = mongoClient.db("uol")
+        db = mongoClient.db('uol')
         const participantsCollection = db.collection('participants')
         const participants = await participantsCollection.find({}).toArray()
-        res.status(200).send(participants)
+        res.send(participants)
+        mongoClient.close()
+    } catch {
+        res.status(400).send('Erro')
+        mongoClient.close()
+    }
+})
+
+app.post('/messages', (req, res) => {
+    res.sendStatus(201)
+})
+
+app.get('/messages', async (req, res) => {
+    try {
+        await mongoClient.connect()
+        db = mongoClient.db('uol')
+        const messagesCollection = db.collection('messages')
+        const messages = await messagesCollection.find({}).toArray()
+        res.send(messages)
         mongoClient.close()
     } catch {
         res.status(400).send('Erro')
