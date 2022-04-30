bb6a76b7e2963944b0772fe6389866ae8fa81a75
Alyson Consuli <consulialyson@gmail.com>
2022-04-30 11:49:16 -0300

    fix: iniciar mongo globalmente

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { json } from 'express'
 import cors from 'cors'
 import { MongoClient } from 'mongodb'
-import dotenv, { parse } from 'dotenv'
+import dotenv from 'dotenv'
 import dayjs from 'dayjs'
 
 const app = express()
@@ -11,12 +11,12 @@ dotenv.config()
 
 let db = null
 const mongoClient = new MongoClient(process.env.MONGO_URI)
+const promise = mongoClient.connect()
+promise.then(() => db = mongoClient.db('uol'))
 
 app.post('/participants', async (req, res) => {
     try {
         const { name } = req.body
-        await mongoClient.connect()
-        db = mongoClient.db('uol')
         const participants = db.collection('participants')
         const messages = db.collection('messages')
         await participants.insertOne({
@@ -31,24 +31,18 @@ app.post('/participants', async (req, res) => {
             time: dayjs().format('HH:mm:ss')
         })
         res.sendStatus(201)
-        mongoClient.close()
     } catch {
         res.status(400).send('Erro')
-        mongoClient.close()
     }
 })
 
 app.get('/participants', async (req, res) => {
     try {
-        await mongoClient.connect()
-        db = mongoClient.db('uol')
         const participantsCollection = db.collection('participants')
         const participants = await participantsCollection.find({}).toArray()
         res.send(participants)
-        mongoClient.close()
     } catch {
         res.status(400).send('Erro')
-        mongoClient.close()
     }
 })
 
@@ -56,8 +50,6 @@ app.post('/messages', async (req, res) => {
     try {
         const { to, text, type } = req.body
         const { user: from } = req.headers
-        await mongoClient.connect()
-        db = mongoClient.db('uol')
         const messages = db.collection('messages')
         await messages.insertOne({
             from,
@@ -67,10 +59,8 @@ app.post('/messages', async (req, res) => {
             time: dayjs().format('HH:mm:ss')
         })
         res.sendStatus(201)
-        mongoClient.close()
     } catch {
         res.status(400).send('Erro')
-        mongoClient.close()
     }
 })
 
@@ -78,8 +68,6 @@ app.get('/messages', async (req, res) => {
     try {
         const limit = parseInt(req.query.limit)
         const { user: from } = req.headers
-        await mongoClient.connect()
-        db = mongoClient.db('uol')
         const messagesCollection = db.collection('messages')
         //const messages = await messagesCollection.find({ to: { $in: ['Todos', 'Teste'] } }).toArray()
         const messages = await messagesCollection.find({}).toArray()
@@ -88,18 +76,14 @@ app.get('/messages', async (req, res) => {
             res.send(messagesLimit)
         }
         (!limit && res.send(messages))
-        mongoClient.close()
     } catch {
         res.status(400).send('Erro')
-        mongoClient.close()
     }
 })
 
 app.post('/status', async (req, res) => {
     try {
         const { user } = req.headers
-        await mongoClient.connect()
-        db = mongoClient.db('uol')
         const participants = db.collection('participants')
         //const participant = await participants.findOne({ name: user })
         await participants.updateOne(
@@ -107,17 +91,13 @@ app.post('/status', async (req, res) => {
             { $set: { lastStatus: Date.now() } }
         )
         res.sendStatus(200)
-        mongoClient.close()
     } catch {
         res.status(400).send('Erro')
-        mongoClient.close()
     }
 })
 
 setInterval(async () => {
     try {
-        await mongoClient.connect()
-        db = mongoClient.db('uol')
         const participantsCollection = db.collection('participants')
         const participants = await participantsCollection.find({}).toArray()
         const messages = db.collection('messages')
@@ -135,10 +115,8 @@ setInterval(async () => {
             }
         }
         //console.log('Remove users inactive')
-        mongoClient.close()
     } catch {
         console.log('Erro')
-        mongoClient.close()
     }
 }, 15000)
 
